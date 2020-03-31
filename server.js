c3e50f34e2d069463a7cbcb49d16ab3c5a52446f
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-03-31 15:41:38 -0400

    Added routes

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get("/", (req,res) => {
     res.send("API running...")
 })
 
+// Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
+
 const PORT= process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
