95726e2cd87b36f45d88b1f3d612f23afef38e13
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-04-02 11:59:17 -0400

    Some small change

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const db= config.get('mongoURI');
 const connectDB= async () =>{
     try{
         await mongoose.connect(db,{ useNewUrlParser: true,
-            useUnifiedTopology: true });
+            useUnifiedTopology: true,
+        useCreateIndex:true });
         console.log("MongoDB connected...");
     }catch(err){
         console.error(err.message);
