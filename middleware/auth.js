de30da9d5c201eacfae836525b6f7f93c0954005
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-04-05 16:53:59 -0400

    Added jwt verify middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req,res,next) => {
+    // Get token from header
+    const token = req.header("x-auth-token");
+
+    // If no token
+    if(!token){
+        res.status(401).json({msg:"No token"});
+    }
+    // Verify token
+    try {
+        const decoded = jwt.verify(token,config.get("jwtSecret"));
+        req.user = decoded.user;
+        next()
+    } catch (error) {
+        res.status(401).json({msg:"Token not valid"})
+    }
+}
