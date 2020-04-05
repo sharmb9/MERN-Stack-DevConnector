efe98383044a2b3f6fcccec8863d240d8400aa12
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-04-05 16:54:40 -0400

    Fixed db issues

--- config/db.js
+++ config/db.js
@@ -1,20 +1,22 @@
-const mongoose= require("mongoose");
-const config=require("config");
-const db= config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 // Async await function
 // Waits for mongoose to get connected to mongoURI
-const connectDB= async () =>{
-    try{
-        await mongoose.connect(db,{ useNewUrlParser: true,
-            useUnifiedTopology: true,
-        useCreateIndex:true });
-        console.log("MongoDB connected...");
-    }catch(err){
-        console.error(err.message);
-        // End the process
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    // End the process
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
