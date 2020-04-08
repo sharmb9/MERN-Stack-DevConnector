7092216a2445fe0dc85b135a7dadd6d941bed1d5
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-04-07 20:52:01 -0400

    Added user authentication and login route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,23 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-module.exports = (req,res,next) => {
-    // Get token from header
-    const token = req.header("x-auth-token");
+module.exports = (req, res, next) => {
+  // Get token from header
+  const token = req.header("x-auth-token");
 
-    // If no token
-    if(!token){
-        res.status(401).json({msg:"No token"});
-    }
-    // Verify token
-    try {
-        const decoded = jwt.verify(token,config.get("jwtSecret"));
-        req.user = decoded.user;
-        next()
-    } catch (error) {
-        res.status(401).json({msg:"Token not valid"})
-    }
-}
+  // If no token
+  if (!token) {
+    res.status(401).json({ msg: "No token, authorization denied!" });
+  }
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"))
+    // Assign the payload to the req.user
+    //console.log(decoded);
+    req.user = decoded.user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({msg: "Token not valid" });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,78 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
 
 // @route GET api/auth
 // @desc TEST route
 // @access Public
-router.get ("/", (req,res)=>{res.send('Auth route')});
+router.get("/", auth, async (req, res) => {
+  try {
+    //   Query user from the db using current user id(check auth.js), excluding the user password
+    const user = await User.findById(req.user.id).select("-password");
+    res.json({ user });
+  } catch (error) {
+    res.json({ msg: error.msg });
+  }
+});
 
-module.exports=router
+router.post(
+  "/",
+  [
+    // checks if email is valid
+    check("email", "Email is required").isEmail(),
+    // checks if password is min 5 characters
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    // Finds the validation errors in request body and wraps them in an object
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      //   assign the user payload for the token, identified by the user id
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //   Sign the user token  using payload and secret id
+      // Send the token to client/user
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+        console.error(error.msg);
+        return res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
