2146368d096f472d2ed01d4d15e6208006411017
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-04-09 11:18:08 -0400

    Updated the api/profile route to create/update a profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,10 +39,91 @@ router.post(
     ],
   ],
   async (req, res) => {
-      const errors = validationResult(req);
-      if(errors){
-          return res.status(500).json({errors:errors.array()});
+    const errors = validationResult(req);
+    if (errors) {
+      return res.status(500).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      status,
+      skills,
+      bio,
+      githubusername,
+      youtube,
+      instagram,
+      facebook,
+      linkedin,
+      twitter,
+    } = req.body;
+
+    //   Build profile object
+    const profileFields = {};
+    //   from the payload
+    profileFields.user = req.user.id;
+    if (company) {
+      profileFields.company = company;
+    }
+    if (website) {
+      profileFields.website = website;
+    }
+    if (location) {
+      profileFields.location = location;
+    }
+    if (status) {
+      profileFields.status = status;
+    }
+    if (bio) {
+      profileFields.bio = bio;
+    }
+    if (githubusername) {
+      profileFields.githubusername = githubusername;
+    }
+    if (status) {
+      profileFields.status = status;
+    }
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    //   Build social object
+    profileFields.social = {};
+    if (youtube) {
+      profileFields.social.youtube = youtube;
+    }
+    if (facebook) {
+      profileFields.social.facebook = facebook;
+    }
+    if (instagram) {
+      profileFields.social.instagram = instagram;
+    }
+    if (twitter) {
+      profileFields.social.twitter = twitter;
+    }
+
+    try {
+      let profile = Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // Find and update profile
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json({profile});
       }
+    //   If no profile in db, create one
+    profile = new Profile({profileFields});
+    await profile.save();
+    return res.json(profile);
+
+
+    } catch (error) {
+        console.log(error.msg);
+        res.status(500).send('Server Error');
+    }
   }
 );
 
