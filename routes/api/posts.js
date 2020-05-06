7577fbb2e5da43f8ba3157ecd72d1b9305c0e80b
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-05-06 03:49:16 -0400

    Added route and model for posts api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,38 @@
-const express = require('express');
+const express = require("express");
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
+const User = require("../../models/User");
+const Post = require("../../models/Posts");
 
-// @route GET api/posts
-// @desc TEST route
-// @access Public
-router.get ("/", (req,res)=>{res.send('Posts route')});
+// @route POST api/posts
+// @desc Create a post
+// @access private
+router.post(
+  "/",
+  [auth, check("text", "Please enter the text for the post").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({erros:errors.array()});
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
 
-module.exports=router
+      const post = await newPost.save();
+
+      res.json(newPost);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json("Server error, checks logs");
+    }
+  }
+);
+
+module.exports = router;
