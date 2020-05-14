a50d008f2c0940f22b35de979db4e1f77ec2ddc6
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-05-14 03:09:07 -0400

    Added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import React, {Fragment} from 'react';
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
-import './App.css';
+import React, { Fragment } from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import "./App.css";
 
-const App = () =>(
-  <Fragment>
-      <Navbar/>
-      <Landing/>
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/Register" component={Register} />
+          <Route exact path="/Login" component={Login} />
+        </Switch>
+      </section>
     </Fragment>
-)
-
-
+  </Router>
+);
 
 export default App;
